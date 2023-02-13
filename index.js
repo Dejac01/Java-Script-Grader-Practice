882ba0bc38454fa207d2bfa9118fdafa3ca42efc
Deja Craft <122240194+Dejac01@users.noreply.github.com>
2023-02-13 18:17:10 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+//Breakout Room Assignment Write an if/else statement for the following requirements: 
+// If Student gets 90 or higher: console log A
+// If students get 80 or above: console log B
+// If students get 70 or above: console log C
+// If students get 55 or above: console log D
+// Any grade lower than 55 is F
+
+let grade = 90
+
+ if (grade >= 90)
+      { console.log (' A')  }
+   
+      else if (grade >= 80)
+        { console.log (' B ')  }
+      
+        
+      else if (grade >= 70)
+      { console.log (' C')  }
+        
+      else if (grade >= 50)
+     { console.log ('D')  }
+        
+      else { console.log ('F')  }
+      
+
